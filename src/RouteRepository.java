540185a36790c16a0c058cb891a7773469afe33e
VladPopa616 <vpopa18@gmail.com>
2026-05-09 12:12:36 +0300

    Added JPA repositories

---
+++ src/RouteRepository.java
@@ -0,0 +1,4 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface RouteRepository extends JpaRepository<Route, Integer> {}
